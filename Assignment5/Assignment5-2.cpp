8f307a27b23df743919a9e0b204a65eed490340b
Firstoly <139094262+Firstoly@users.noreply.github.com>
2023-10-29 16:36:43 +0700

    Add files via upload

---
+++ Assignment5/Assignment5-2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void CheckNum(int num,int ran);
+
+int main ()
+{   
+
+    // เขียนโปรแกรมเพื่อทายเลขโดยรับค่าตัวเลขจนกว่าจะทายถูก
+
+
+
+    int number,random,value=0;
+    srand (time(NULL));
+    random =  (rand()%10)+1;        
+    cout << "### Welecome to guessing number game ###" << endl;
+    cout << "Secret number has been chosen"  << endl;
+    do
+    {   
+        cout << "Guess the number (1 to 10) : ";
+        cin >> number; 
+        value ++;
+        CheckNum(number,random);
+    }while (number != random); 
+
+    cout << "\nCongratulations!"<< endl;  
+    cout << "The secret number is "<< random <<endl;
+    cout << "You made "<< value << " guesses"<<endl;  
+
+    system ("pause");
+    return 0;
+}
+
+void CheckNum(int num,int ran)
+{
+    int number=num,random=ran;
+   
+        if (number < random)
+            cout << "The secret number is lower" <<endl;
+        else if (number > random)
+            cout << "The secret number is higher" <<endl;       
+}
