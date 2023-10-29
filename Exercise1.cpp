7769b265d6b0504376e2eb3ce663aac1779fb5a4
Firstoly <139094262+Firstoly@users.noreply.github.com>
2023-10-29 14:29:50 +0700

    Add files via upload

---
+++ Exercise1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Student Code : 66-060216-3103-3\n";
+    cout << "Student Name : Ntchapak Poim\n";
+    cout << "    Division : Information Technology\n";
+    cout << "        Year : 1\n";
+    cout << "        Room : RB\n";
+    cout << "         Sex : Male\n";
+    cout << "         Age : 18\n";
+return 0;
+}
